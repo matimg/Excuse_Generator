af05f86cda30af9ec29521a395abeb068b1dd3ea
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-13 11:00:10 +0000

    Excuse generator implementado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog ", "my granma ", "his turtle ", "my bird "];
+  let what = ["eat ", "pissed ", "crushed ", "broked "];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var r1 = Math.floor(Math.random() * who.length);
+  var r2 = Math.floor(Math.random() * what.length);
+  var r3 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML = who[r1] + what[r2] + when[r3];
 };

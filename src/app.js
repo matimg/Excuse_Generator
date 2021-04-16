e6528d87c6ddbf2205e6f7e572d4b33244b0ebbf
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-16 13:45:17 +0000

    Trabajo mejorado

--- src/app.js
+++ src/app.js
@@ -5,16 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+//Llamo funcion al cargar la página.
+window.onload = excuseGenerator();
+//Llamo funcion al hacer click en el botón.
+document.getElementById("button").addEventListener("click", excuseGenerator);
+
+function excuseGenerator() {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
+  let action = [" ate", " peed", " crushed", " broke"];
+  let what = [" my homework", " the keys", " the car"];
   let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
+    " before the class",
+    " right on time",
+    " when I finished",
+    " during my lunch",
+    " while I was praying"
   ];
 
   var r1 = Math.floor(Math.random() * who.length);
@@ -24,4 +29,4 @@ window.onload = function() {
 
   document.querySelector("#excuse").innerHTML =
     who[r1] + action[r2] + what[r3] + when[r4];
-};
+}

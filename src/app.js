10c97098b649406e138ed1b49a66765bae9534ae
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-16 13:16:56 +0000

    Excuse generator terminado

--- src/app.js
+++ src/app.js
@@ -6,19 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let who = ["the dog ", "my granma ", "his turtle ", "my bird "];
-  let what = ["eat ", "pissed ", "crushed ", "broked "];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
   let when = [
     "before the class",
-    "right in time",
+    "right on time",
     "when I finished",
     "during my lunch",
     "while I was praying"
   ];
 
   var r1 = Math.floor(Math.random() * who.length);
-  var r2 = Math.floor(Math.random() * what.length);
-  var r3 = Math.floor(Math.random() * when.length);
+  var r2 = Math.floor(Math.random() * action.length);
+  var r3 = Math.floor(Math.random() * what.length);
+  var r4 = Math.floor(Math.random() * when.length);
 
-  document.querySelector("#excuse").innerHTML = who[r1] + what[r2] + when[r3];
+  document.querySelector("#excuse").innerHTML =
+    who[r1] + action[r2] + what[r3] + when[r4];
 };
